d365402ad38194c8dfcceec67577d1b5fbbe0b42
Pandarock0 <151137645+Pandarock0@users.noreply.github.com>
2025-03-27 14:56:08 +0100

    Update Minimize function

--- src/minimize.py
+++ src/minimize.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
 from FiniteAutomata import FiniteAutomata
 
-
 def minimize(fa):
-    # Hopcroft's algorithm (simplified)
+    def group_name(group):
+        return '.'.join(map(str, sorted(group)))
+
+    # Étape 1 : créer les partitions initiales (finales vs non-finales)
     partitions = set()
     non_final = frozenset(fa.states - fa.final_states)
     final = frozenset(fa.final_states)
@@ -11,17 +13,20 @@ def minimize(fa):
         partitions.add(final)
     if non_final:
         partitions.add(non_final)
+
+    # Étape 2 : Raffiner les partitions
     while True:
         new_partitions = set()
         for group in partitions:
             split = defaultdict(set)
             for state in group:
+                # Signature = vers quelles partitions pointent les transitions
                 key = tuple()
                 for sym in sorted(fa.alphabet):
                     tgt = next(iter(fa.get_transitions(state, sym)), None)
                     for p in partitions:
                         if tgt in p:
-                            key += (p,)
+                            key += (frozenset(p),)
                             break
                 split[key].add(state)
             for s in split.values():
@@ -29,24 +34,31 @@ def minimize(fa):
         if new_partitions == partitions:
             break
         partitions = new_partitions
+
+    # Étape 3 : Création de l'automate minimal
     state_to_group = {}
     for group in partitions:
         for state in group:
             state_to_group[state] = group
-    new_states = partitions
-    new_initial = next(iter(state_to_group[next(iter(fa.initial_states))]))
-    new_final = set()
-    for group in partitions:
-        if any(s in fa.final_states for s in group):
-            new_final.add(group)
+
     new_fa = FiniteAutomata()
     new_fa.alphabet = fa.alphabet.copy()
-    new_fa.initial_states = {str(new_initial)}
-    new_fa.final_states = {str(g) for g in new_final}
-    new_fa.states = {str(g) for g in partitions}
+    new_fa.states = {group_name(group) for group in partitions}
+
+    init_group = state_to_group[next(iter(fa.initial_states))]
+    new_fa.initial_states = {group_name(init_group)}
+
+    new_fa.final_states = {
+        group_name(group) for group in partitions if group & fa.final_states
+    }
+
     for group in partitions:
+        rep_state = next(iter(group))
         for sym in fa.alphabet:
-            tgt_state = next(iter(fa.get_transitions(next(iter(group)), sym)))
-            tgt_group = state_to_group[tgt_state]
-            new_fa.add_transition(str(group), sym, str(tgt_group))
+            tgts = fa.get_transitions(rep_state, sym)
+            if tgts:
+                tgt = next(iter(tgts))
+                tgt_group = state_to_group[tgt]
+                new_fa.add_transition(group_name(group), sym, group_name(tgt_group))
+
     return new_fa
